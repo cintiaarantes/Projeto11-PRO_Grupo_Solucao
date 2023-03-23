8c48f84a296d01b3ac2a46c9fa7f59c4e497c157
Cíntia Arantes <114604292+cintiaarantes@users.noreply.github.com>
2023-03-23 08:24:40 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var shipImag;
+var seaImag;
+var seaSprite;
+var shipSprite;
+
+
+function preload() {
+  seaImag = loadImage("sea.png");
+  shipImag = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  seaSprite = createSprite(400,200);
+  seaSprite.addImage(seaImag);
+  seaSprite.scale=0.3;
+  seaSprite.velocityX = -3;
+
+  shipSprite = createSprite(130,200);
+  shipSprite.addAnimation("navigation", shipImag);
+  shipSprite.scale=0.25;
+
+}
+
+function draw() {
+  background("blue");
+  
+  if (seaImag.x < 0) {
+    seaImag.x = sea.witdh / 2;
+    seaImag.x = sea.witdh / 2
+  }
+
+  drawSprites();
+}
